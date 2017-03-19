544efa5c31dc4a97c29ffe908532296a9868ee95
Finch <finch@play-net.org>
2017-03-20 00:57:46 +0100

    Add some more tests

--- rcon/packets_test.go
+++ rcon/packets_test.go
@@ -2,11 +2,50 @@ package rcon
 
 import "testing"
 
-func Test_stripHeader(t *testing.T) {
-	cmd := "Kick steve"
-	pck := buildPacket([]byte(cmd), packetType.Command)
-	result := stripHeader(pck)
-	if string(result[2:]) != cmd {
-		t.Fatal("Cmd Mismatch:", cmd, "-", string(result))
+func Test_getSequence(t *testing.T) {
+	var tests = []struct {
+		test     []byte
+		expected byte
+	}{
+		{
+			test:     []byte{'B', 'E', 1, 1, 1, 1, 0, 1, 255, 10, 5, 2, 82},
+			expected: 255,
+		},
+		{
+			test:     []byte{'B', 'E', 1, 1, 1, 1, 0, 1, 85},
+			expected: 85,
+		},
+	}
+
+	for _, v := range tests {
+		result := getSequence(v.test)
+		if result != v.expected {
+			t.Error("Expected:", v.expected, "Got:", result)
+		}
+	}
+}
+
+func Test_responseType(t *testing.T) {
+	var tests = []struct {
+		test     []byte
+		expected byte
+	}{
+		{
+			test:     []byte{'B', 'E', 1, 1, 1, 1, 0, 1, 255, 10, 5, 2, 82},
+			expected: 1,
+		},
+		{
+			test:     []byte{'B', 'E', 1, 1, 1, 1, 0, 1, 85},
+			expected: 1,
+		},
+	}
+	for _, v := range tests {
+		result, err := responseType(v.test)
+		if err != nil {
+			t.Error("Test:", v.test, "Failed due to error:", err)
+		}
+		if result != v.expected {
+			t.Error("Expected:", v.expected, "Got:", result)
+		}
 	}
 }
